8697d5e708e9d6d6eb9094e071330b71da312af7
praburaj526 <praburaj526@gmail.com>
2022-05-06 13:42:56 +0530

    some lines added

--- src/github/org/Github.java
+++ src/github/org/Github.java
@@ -1,7 +1,13 @@
 package github.org;
 
 public class Github {
+int a=10;
+int b=5;
+ int c=a+b;
+private void sysout() {
+	System.out.println("c");
 
+}
 	public static void main(String[] args) {
 
 String s ="Hello World";
